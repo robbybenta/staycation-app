572a286bc48ffecf3038cb62b4a78295b4173e3a
robbybenta <robbybenta7@gmail.com>
2021-02-24 18:59:50 +0700

    Development initialize

---
+++ app.js
@@ -0,0 +1,16 @@
+const express=require('express')
+const path = require('path')
+const router=require('./routes/index')
+const app=express()
+const port=3030
+
+
+//req.body
+app.use(express.urlencoded({ extended: true }))
+app.set('view-engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(router)
+
+app.listen(port,()=>{
+    console.log(`aplikasi ini berjalan di ${port}`)
+})
