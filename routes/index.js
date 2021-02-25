a8f0fb498ce1f28b27ab99e8d848384ed6175e96
robbybenta <robbybenta7@gmail.com>
2021-02-25 19:29:24 +0700

    resolve conflict

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,11 @@ const User=require('../controllers/controller-user')
 router.get('/',(req,res)=>{
 res.render('home.ejs')
 })
+router.use('/hotel', hotelRoutes)
+router.get('/register', User.register)
+router.post('/register', User.registerAdd)
+router.get('/login', User.login)
+
 router.get('/register',User.register)
 router.post('/register',User.registerAdd)
 router.get('/login',User.login)
