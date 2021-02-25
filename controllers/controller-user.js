992ecc7fa1c0e4aa9d83459dc2f6499278da77c6
robbybenta <robbybenta7@gmail.com>
2021-02-25 19:26:01 +0700

    robbybenta

--- controllers/controller-user.js
+++ controllers/controller-user.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { comparePassword } = require('../helpers/bcrypt')
 const { user } = require('../models')
 
 
@@ -23,6 +24,24 @@ class Controller {
                 console.log(err)
                 res.send(err.message)
             })
+    }
+    static loginSuccess(req,res){
+        user.findOne({where:{email:req.body.email}})
+        .then((data)=>{
+        //    if(data.password===comparePassword()req.body.password){
+
+        //    }
+        if(comparePassword(req.body.password,data.password)){
+            // res.send('benar')
+            res.render('error.ejs')
+        }
+        else{
+            res.render('error.ejs')
+        }
+        })
+        .catch((err)=>{
+            res.send(err.message)
+        })
     }
 }
 
